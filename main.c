845d58009449123fc9e6adb84681150fc648a4c3
fatmaaymanm <fatma.aymanm@gmail.com>
2024-03-28 01:06:27 +0200

    Test Files Added

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(void) {
+//    int x = 10;
+//    // This is a comment
+//    float y = 3.14 + x; // This is another comment
+//    char initial = 'A \n';
+//    float iffy = 2.5 * y;
+//    const char *i = "this is\ta string";
+//    puts("this is\ta string");
+//    return 0;
+//}
+
---
+++ test1.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//
+//int main() {
+//   int x = 0, y = 1;
+//   int z=x+y;
+//   int arr[10] = {0, 1, 2, 3, 4, 5,6, 7, 8, 9};
+//   int *ptr = &x;
+//   int u = x ? 8 : 9;
+//   int c = 10e+567478;
+//   int d = 10e-567478;
+//   int e = -10e567478;
+//   struct r {
+//           int z;
+//           float v;
+//           char s;
+//   };
+//}
+
+
