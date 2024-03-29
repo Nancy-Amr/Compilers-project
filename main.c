5360ba31109171058442e0b2e9fbdb91bc53f1be
fatmaaymanm <fatma.aymanm@gmail.com>
2024-03-29 18:50:04 +0200

    Phase 1 Final

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main(void) {
-//    int x = 10;
-//    // This is a comment
-//    float y = 3.14 + x; // This is another comment
-//    char initial = 'A \n';
-//    float iffy = 2.5 * y;
-//    const char *i = "this is\ta string";
-//    puts("this is\ta string");
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int x = 10;
+    // This is a comment
+    float y = 3.14 + x; // This is another comment
+    char initial = 'A \n';
+    float iffy = 2.5 * y;
+    const char *i = "this is\ta string";
+    puts("this is\ta string");
+    int x = 0, y = 1; /* ******** MULTILINED COMMENT //////////////// */
+    int jhghj989tghjhgj = 10;
+    int z=x+y;
+    int arr[10] = {0, 1, 2, 3, 4, 5,6, 7, 8, 9};
+    int *ptr = &x;
+    int u = x ? 8 : 9;
+    int c = 10e+567478;
+    int d = 10e-567478;
+    int e = 10e567478;
+    struct r {
+        int z;
+        float v;
+        char s;
+    };
+    string xx->yy = "hello";
+    float zz.oo = 8.809;
+    return 0;
+    float y = 0b01103 + 0b0101 + 078954 + 0756 + 0A + 0 +0 + 0X256A + 0x125N -54 + 0xabc123 - x;
+    string __tyghjk;
+
+}
 
